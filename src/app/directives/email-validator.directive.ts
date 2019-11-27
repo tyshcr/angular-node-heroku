d918d113f454723b997b119fdfe21e5df4564e80
Christopher Tysh <christopher.tysh@slalom.com>
2019-11-27 15:09:54 -0600

    trying to fix directive failure on heroku

--- src/app/directives/email-validator.directive.ts
+++ src/app/directives/email-validator.directive.ts
@@ -1,17 +1,8 @@
-import { Directive } from '@angular/core';
-import { NG_VALIDATORS, FormControl } from '@angular/forms';
-
-function emailValidation(control: FormControl) {
-    let email = control.value
-    let regex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
-    if (email && regex.test(email)) {
-      return null
-    }
-    return { error: true }
-}
+import { Directive } from '@angular/core'
+import { NG_VALIDATORS, FormControl } from '@angular/forms'
 
 @Directive({
-  selector: '[emailValidator][ngModel]',
+  selector: '[ngEmailValidator][ngModel]',
   providers: [{
     provide: NG_VALIDATORS,
     useValue: emailValidation,
@@ -20,4 +11,13 @@ function emailValidation(control: FormControl) {
 })
 export class EmailValidatorDirective {
   constructor() { }
+}
+
+function emailValidation(control: FormControl) {
+    let email = control.value
+    let regex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
+    if (email && regex.test(email)) {
+      return null
+    }
+    return { error: true }
 }
